107c3360edb6443564fa6887327056412cc6d994
Giancarlo Horta <giancarlohorta@gmail.com>
2024-09-18 13:17:46 +0100

    feat(general): implement typescript on routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,9 +1,9 @@
 export const ROUTES = {
   home: "/",
   podcast: "/podcast/:podcastId/*"
-};
+} as const;
 
 export const INTERNAL_ROUTES = {
   episodes: "/",
   episode: "episode/:episodeId"
-};
+} as const;
